a84c560642c0c5dedfb714aafcba16539a77945d
Shubham8799 <106910292+Shubham8799@users.noreply.github.com>
2022-06-26 08:51:34 +0530

    Update project2.py

--- project2.py
+++ project2.py
@@ -17,10 +17,9 @@ Current_age = input("enter your current age : ")
 New_age = int(old_age) + 2
 Print (" So ") + name (" After two years your age will be this ") 
 Print (new_age) 
-Print ( "Is it true/false") 
-True = input("I knew it") 
-False = input(" I am solly  ") 
-True/false = input(" Enter your answer true or false ") 
-Print ("answer") 
-Input (answer) 
+Print ("Is it true/false") 
+
+
+
+
 
